b1cd4d9d3c2bfe297d16f3dbf058e2e8c7b23d71
alexandrtovmach <alexandrtovmach@gmail.com>
2018-10-18 01:14:49 +0300

    replace start() stop() with connection tooglers

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 export default class BgTimerHelper {
   constructor() {
     this.aCtx = new AudioContext();
-    this.src = this.aCtx.createConstantSource();
-    this.gainRef = this.aCtx.createGain();
-    this.gainRef.gain.value = 0.01;
-    this.src.connect(this.gainRef);
-    this.gainRef.connect(this.aCtx.destination);
+    this.track = this.aCtx.createConstantSource();
+    this.gainNode = this.aCtx.createGain();
+    this.gainNode.gain.value = 0.005;
+    this.gainNode.connect(this.aCtx.destination);
   }
 
   enable() {
-    this.src.start();
+    this.track.connect(this.aCtx.destination);
   }
 
   disable() {
-    this.src.stop();
+    this.track.disconnect(this.aCtx.destination);
   }
 }
